b394a4db5ea6d2552f53686859f8148b43cb93c8
grestorv <vertoserk@yandex.by>
2020-04-13 23:01:42 +0300

    Добавил сохранение данныз в edit

--- templates/edit.php
+++ templates/edit.php
@@ -2,10 +2,14 @@
 
 <?php
 $id=$data['id'];
-$name=$data['name'];
-$email=$data['email'];
-$text=$data['text'];
-$complete=$data['complete'];
+if(isset($_COOKIE['name'])) $name=$_COOKIE['name'];
+else $name=$data['name'];
+if(isset($_COOKIE['email'])) $email=$_COOKIE['email'];
+else $email=$data['email'];
+if(isset($_COOKIE['text'])) $text=$_COOKIE['text'];
+else $text=$data['text'];
+if(isset($_COOKIE['complete'])) $complete=$_COOKIE['complete'];
+else $complete=$data['complete'];
 if($complete==0) $checked='';
 else $checked=' checked';
 ?>
@@ -24,15 +28,40 @@ else $checked=' checked';
 					<span class="text">Новая задача</span>
 			<div id="form">
 				<form action="#form" method="POST">
-					<input class="form-control" type="hidden" name="id" value="<?=$id?>">
-					<input class="form-control" type="text" name="name" value="<?=$name?>">
-					<input class="form-control" type="text" name="email" value="<?=$email?>">
-					<textarea class="form-control" name="text"><?=$text?></textarea>
+					<input id="id" class="form-control" type="hidden" name="id" value="<?=$id?>">
+					<input id="name" class="form-control" type="text" name="name" value="<?=$name?>">
+					<input id="email" class="form-control" type="text" name="email" value="<?=$email?>">
+					<textarea id="text" class="form-control" name="text"><?=$text?></textarea>
 					<p><span class="text">Выполнено</span>
-					<input type="checkbox" class="form-check-input" name="complete" value="1"<?=$checked?>>
+					<input id="complete" type="checkbox" class="form-check-input" name="complete" value="1"<?=$checked?>>
 					</p>
 					<input type="submit" class="btn btn-info btn-block" value="Сохранить">
 				</form>
 			</div>
 		</body>
+			<script>
+		let elem1 = document.getElementById('name');
+		elem1.oninput = function() {
+     		document.cookie = "name="+elem1.value;
+   		};
+
+		let elem2 = document.getElementById('email');
+		elem2.oninput = function() {
+     		document.cookie = "email="+elem2.value;
+   		};
+
+   		let elem3 = document.getElementById('text');
+		elem3.oninput = function() {
+     		document.cookie = "text="+elem3.value;
+   		};
+   		let elem4 = document.getElementById('complete');
+		elem4.onchange = function() {
+			if (elem4.checked) {
+     		document.cookie = "complete=1";
+     		}
+     		else document.cookie = "complete=0";
+   		};
+
+
+	</script>
 </html>
